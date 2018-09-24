9128e19b48f57d6dab6a8047f0f02be60180de40
Amy Ramirez <amyramirez@Amys-MacBook-Pro.local>
2018-09-23 17:16:56 -0700

    added login

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 valid_users = ['Randy', 'Amy', 'Yo', 'Amy']
@@ -12,6 +12,16 @@ def hello():
 @app.route("/goodbye")
 def goodbye():
     return render_template('goodbye.html', users=valid_users)
+
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if request.form['name'] in valid_users:
+            return render_template('hello.html', user=request.form['name'])
+        else:
+            return render_template('goodbye.html')
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
